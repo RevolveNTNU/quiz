14a658c750670975efc04406cebdf5d594a11ebf
Simen <simennj@outlook.com>
2017-12-13 14:16:40 +0100

    make name of tag pk slug

--- quiz/models.py
+++ quiz/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Tag(models.Model):
-    name = models.CharField(max_length=16, primary_key=True)
+    name = models.SlugField(max_length=16, primary_key=True)
 
     def __str__(self):
         return self.name
