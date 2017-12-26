5ad1557c8a24be0e857b4826f44c7e8d68493ea5
Simen <simennj@outlook.com>
2017-12-26 13:02:18 +0100

    add admin entries for models

--- quiz/admin.py
+++ quiz/admin.py
@@ -1,1 +1,19 @@
-# Register your models here.
+from django.contrib import admin
+
+from quiz.models import Quiz, Question, Answer
+
+
+@admin.register(Quiz)
+class QuizAdmin(admin.ModelAdmin):
+    filter_horizontal = ['questions']
+
+
+class AnswerInline(admin.StackedInline):
+    model = Answer
+    extra = 0
+
+
+@admin.register(Question)
+class QuizAdmin(admin.ModelAdmin):
+    filter_horizontal = ['tags']
+    inlines = [AnswerInline]
